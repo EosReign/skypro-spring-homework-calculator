ee1271c925c1f8fbe5ccba214becad69f32722c0
EosReign <107928418+EosReign@users.noreply.github.com>
2022-09-21 16:32:15 +0300

    .

--- src/main/java/pro/sky/skyprospringhomeworkcalculator/CalculatorController.java
+++ src/main/java/pro/sky/skyprospringhomeworkcalculator/CalculatorController.java
@@ -16,7 +16,7 @@ public class CalculatorController {
     @GetMapping(path = "/calculator/plus")
     public String plus(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
         if (num1 != 0 && num2 != 0) {
-            return CalculatorService.plus(num1, num2);
+            return num1 + " + " + num2 + " = " + CalculatorService.plus(num1, num2);
         } else {
             return "Error";
         }
@@ -24,7 +24,7 @@ public class CalculatorController {
     @GetMapping(path = "/calculator/minus")
     public String minus(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
         if (num1 != 0 && num2 != 0) {
-            return CalculatorService.minus(num1, num2);
+            return num1 + " - " + num2 + " = " + CalculatorService.minus(num1, num2);
         } else {
             return "Error";
         }
@@ -32,7 +32,7 @@ public class CalculatorController {
     @GetMapping(path = "/calculator/multiply")
     public String multiply(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
         if (num1 != 0 && num2 != 0) {
-            return CalculatorService.multiply(num1, num2);
+            return num1 + " * " + num2 + " = " + CalculatorService.multiply(num1, num2);
         } else {
             return "Error";
         }
@@ -40,7 +40,7 @@ public class CalculatorController {
     @GetMapping(path = "/calculator/divide")
     public String divide(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
         if (num1 != 0 && num2 != 0) {
-            return CalculatorService.divide(num1, num2);
+            return num1 + " / " + num2 + " = " + CalculatorService.divide(num1, num2);
         } else {
             return "Error";
         }
--- src/main/java/pro/sky/skyprospringhomeworkcalculator/CalculatorService.java
+++ src/main/java/pro/sky/skyprospringhomeworkcalculator/CalculatorService.java
@@ -5,16 +5,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Service
 public class CalculatorService {
-    public static String plus(int num1, int num2) {
-        return num1 + " + " + num2 + " = " + (num1 + num2);
+    public static int plus(int num1, int num2) {
+        return num1 + num2;
     }
-    public static String minus(int num1, int num2) {
-        return num1 + " - " + num2 + " = " + (num1 - num2);
+    public static int minus(int num1, int num2) {
+        return num1 - num2;
     }
-    public static String multiply(int num1, int num2) {
-        return num1 + " * " + num2 + " = " + (num1 * num2);
+    public static int multiply(int num1, int num2) {
+        return num1 * num2;
     }
-    public static String divide(int num1, int num2) {
-        return num1 + " / " + num2 + " = " + (num1 / num2);
+    public static int divide(int num1, int num2) {
+        return num1 / num2;
     }
 }
