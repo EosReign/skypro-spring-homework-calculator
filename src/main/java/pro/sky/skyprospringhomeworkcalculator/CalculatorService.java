2a2c24a6cb5356e3eb959ce81bc9504e84885d47
EosReign <107928418+EosReign@users.noreply.github.com>
2022-09-21 16:54:10 +0300

    .

--- src/main/java/pro/sky/skyprospringhomeworkcalculator/CalculatorService.java
+++ src/main/java/pro/sky/skyprospringhomeworkcalculator/CalculatorService.java
@@ -5,16 +5,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Service
 public class CalculatorService {
-    public static int plus(int num1, int num2) {
+    public int plus(int num1, int num2) {
         return num1 + num2;
     }
-    public static int minus(int num1, int num2) {
+    public int minus(int num1, int num2) {
         return num1 - num2;
     }
-    public static int multiply(int num1, int num2) {
+    public int multiply(int num1, int num2) {
         return num1 * num2;
     }
-    public static int divide(int num1, int num2) {
+    public int divide(int num1, int num2) {
         return num1 / num2;
     }
 }
