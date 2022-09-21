1b1823ff1b32ffc863c054c99978072caba5fdea
EosReign <107928418+EosReign@users.noreply.github.com>
2022-09-21 16:56:35 +0300

    .

--- src/main/java/pro/sky/skyprospringhomeworkcalculator/CalculatorService.java
+++ src/main/java/pro/sky/skyprospringhomeworkcalculator/CalculatorService.java
@@ -1,7 +1,6 @@
 package pro.sky.skyprospringhomeworkcalculator;
 
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @Service
 public class CalculatorService {
